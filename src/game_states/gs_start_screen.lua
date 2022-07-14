32b5e94a6da888e10dcec45c231e5e4bf1524b68
Beetroot Paul <paul@beetrootpaul.com>
2022-07-14 23:36:08 +0200

    info about key to toggle debug layer

--- src/game_states/gs_start_screen.lua
+++ src/game_states/gs_start_screen.lua
@@ -17,14 +17,20 @@ function new_gs_start_screen:draw()
     local text_gap = 6
     local text_1 = "breakout"
     local text_2 = "press ❎ to start"
+    local text_3 = "🅾️ toggles in-game debug layer"
     local text_1_w = u.measure_text_width(text_1)
     local text_2_w = u.measure_text_width(text_2)
+    local text_3_w = u.measure_text_width(text_3)
     print(text_1,
         u.screen_edge_length / 2 - text_1_w / 2,
-        u.screen_edge_length / 2 - text_gap / 2 - u.text_height_px,
+        u.screen_edge_length / 2 - u.text_height_px / 2 - text_gap - u.text_height_px,
         u.colors.yellow)
     print(text_2,
         u.screen_edge_length / 2 - text_2_w / 2,
-        u.screen_edge_length / 2 + text_gap / 2,
+        u.screen_edge_length / 2 - u.text_height_px / 2,
         u.colors.light_grey)
+    print(text_3,
+        u.screen_edge_length / 2 - text_3_w / 2,
+        u.screen_edge_length / 2 + u.text_height_px / 2 + text_gap,
+        u.colors.dark_grey)
 end
